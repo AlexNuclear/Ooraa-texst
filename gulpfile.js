30f3ebf6cdd8c1c115e0062df77a887cc063f4f9
Postnikov Alex <Peaceatom@bk.ru>
2017-06-26 10:13:48 +0300

    done

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("style", function() {
+  gulp.src("sass/style.sass")
+    .pipe(plumber())
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 2 versions"
+      ]})
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.sass", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
